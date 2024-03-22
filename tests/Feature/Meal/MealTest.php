3eedc824c177c5863dfb41e0937b41090b8f8294
mehennib <mehenni.badreddine1@gmail.com>
2024-03-22 10:30:39 +0100

    Edit test

--- tests/Feature/Meal/MealTest.php
+++ tests/Feature/Meal/MealTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Meal;
 
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class MealTest extends TestCase
@@ -13,6 +14,13 @@ class MealTest extends TestCase
      */
     public function testMealsEndpoint()
     {
+        Http::fake([
+            '*' => Http::response(['meals' => [
+                ['idMeal' => '1', 'strMeal' => 'Meal 1', 'strCategory' => 'Category 1', 'strArea' => 'Area 1', 'strInstructions' => 'Instructions 1', 'strMealThumb' => 'Image 1'],
+                ['idMeal' => '2', 'strMeal' => 'Meal 2', 'strCategory' => 'Category 2', 'strArea' => 'Area 2', 'strInstructions' => 'Instructions 2', 'strMealThumb' => 'Image 2'],
+            ]], 200),
+        ]);
+
         $response = $this->get(route('meals.index'));
 
         $response->assertStatus(200);
