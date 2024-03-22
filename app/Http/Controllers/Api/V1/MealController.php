f87a4c895b612c86c933f60bd2b6a04be349d16f
mehennib <mehenni.badreddine1@gmail.com>
2024-03-22 11:03:48 +0100

    Add documenation for fetch meals function

--- app/Http/Controllers/Api/V1/MealController.php
+++ app/Http/Controllers/Api/V1/MealController.php
@@ -8,6 +8,11 @@ use App\Facades\Meal as MealService;
 
 class MealController extends Controller
 {
+    /**
+     * Display a listing of the meals.
+     *
+     * @return \Illuminate\Http\JsonResponse A JSON response containing the fetched meals or an error message.
+     */
     public function index()
     {
         return MealService::fetchMeals();
